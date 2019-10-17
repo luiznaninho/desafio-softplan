e90fb9509229745222cfa935a0b3847eca407bc2
Luiz Ernani - Naninho <luizernanijr@gmail.com>
2019-10-17 17:29:37 -0300

    #1 - Inicio API_2.

---
+++ DesafioSoftplan.API_2/Controllers/CalculaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioSoftplan.API_2.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("calculaJuros")]
+        public ActionResult<double> Get(double valorInicial, int meses)
+        {
+            return valorInicial * meses;
+        }
+
+        [HttpGet]
+        [Route("showmethecode")]
+        public ActionResult<string> ShowMeTheCode()
+        {
+            return "https://github.com/luiznaninho/desafio-softplan";
+        }
+    }
+}
